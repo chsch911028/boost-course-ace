f2450641224462206d2a8f4ea286a987a7b94a95
seongchanchoi <chsch911028@gmail.com>
2019-07-28 16:44:37 +0900

    Fix gnb confirm reservation to know login state

--- reservation.v1.5/resevation-html-base/js/index.js
+++ reservation.v1.5/resevation-html-base/js/index.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
+  const BOOKING_LOING_PAGE_URL = "./bookinglogin.html";
+  const MY_RESERVATION_PAGE_URL = "./myreservation.html";
+
+  let state = {
+    login: {
+      isLogin: false,
+      loginId: ""
+    }
+  };
+
   //1. 네이버 로고, 예약 버튼 클릭시 반응 없애기
   $(".lnk_logo").click(function(e) {
     e.preventDefault();
   });
+
+  //2. Login 여부에 따라 GNB 상단 우측 상태변환
+  if (state.login.isLogin) {
+    $(".viewReservation").text(state.login.loginId);
+    $(".btn_my").attr("href", MY_RESERVATION_PAGE_URL);
+  }
 });
