3937600a2a112f36b86bd8ec85cc4f5680d2158b
seongchanchoi <chsch911028@gmail.com>
2019-08-23 23:02:25 +0900

    Implement /api/categories 카테고리 목록 구하기

---
+++ src/main/java/kr/or/connect/pj3be/dao/CategoryDao.java
@@ -0,0 +1,34 @@
+package kr.or.connect.pj3be.dao;
+
+import static kr.or.connect.pj3be.sql.CategoryDaoSqls.*;
+import kr.or.connect.pj3be.dto.Category;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class CategoryDao {
+
+    private NamedParameterJdbcTemplate jdbc;
+    private SimpleJdbcInsert insertAction;
+    private RowMapper<Category> rowMapper = BeanPropertyRowMapper.newInstance(Category.class);
+
+    public CategoryDao(DataSource dataSource) {
+        this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+        this.insertAction = new SimpleJdbcInsert(dataSource)
+                .withTableName("category")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    public List<Category> selectAll(){
+        return this.jdbc.query(SELECT_ALL, Collections.emptyMap(), rowMapper);
+    }
+
+}
