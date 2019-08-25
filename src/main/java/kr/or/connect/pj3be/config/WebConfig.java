2f99e6ff47a6d1aa4f72f2c009149e60d36d1d35
seongchanchoi <chsch911028@gmail.com>
2019-08-26 05:13:49 +0900

    Add resourceHandlers for static resources

--- src/main/java/kr/or/connect/pj3be/config/WebConfig.java
+++ src/main/java/kr/or/connect/pj3be/config/WebConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @ComponentScan(basePackages = "kr.or.connect.pj3be", useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
@@ -17,6 +14,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry viewResolverRegistry) {
         viewResolverRegistry.jsp("/WEB-INF/",".jsp");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/WEB-INF/htmls/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/js/").setCachePeriod(31556926);
+        registry.addResourceHandler("/font/**").addResourceLocations("/WEB-INF/font/").setCachePeriod(31556926);
+
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img_map/**").addResourceLocations("/img_map/").setCachePeriod(31556926);
     }
 
     @Override
