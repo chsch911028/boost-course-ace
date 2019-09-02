fcaf91d09cf1222115730f420620ca9385fc4f43
seongchanchoi <chsch911028@gmail.com>
2019-09-02 18:17:29 +0900

    Add getDisplayInfoResponse() in DisplayInfoService interface

--- src/main/java/kr/or/connect/pj3be/service/DisplayInfoService.java
+++ src/main/java/kr/or/connect/pj3be/service/DisplayInfoService.java
@@ -1,4 +1,8 @@
 package kr.or.connect.pj3be.service;
 
+import kr.or.connect.pj3be.dto.displayInfo.DisplayInfoResponse;
+
 public interface DisplayInfoService {
+
+    public DisplayInfoResponse getDisplayInfoResponse(Integer displayInfoId);
 }
