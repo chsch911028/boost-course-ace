060863d8a27a42375aae7994014fd3e32b31391a
seongchanchoi <chsch911028@gmail.com>
2019-09-02 18:18:28 +0900

    Impelment getAllCategories() in CategoryService interface

--- src/main/java/kr/or/connect/pj3be/service/CategoryService.java
+++ src/main/java/kr/or/connect/pj3be/service/CategoryService.java
@@ -1,9 +1,10 @@
 package kr.or.connect.pj3be.service;
 
 import kr.or.connect.pj3be.dto.category.Category;
+import kr.or.connect.pj3be.dto.category.CategoryResponse;
 
 import java.util.List;
 
 public interface CategoryService {
-    public List<Category> getList();
+    public List<Category> getAllCategories();
 }
