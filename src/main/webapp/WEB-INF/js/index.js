3cc3be2fea61434f4ba59903553206782d898bad
seongchanchoi <chsch911028@gmail.com>
2019-09-03 01:06:53 +0900

    Remove Carousel code & Make api-url to have constants variables

--- src/main/webapp/WEB-INF/js/index.js
+++ src/main/webapp/WEB-INF/js/index.js
@@ -9,111 +9,6 @@ $(document).ready(function() {
     }
   };
 
-  //[참고 블로그] https://im-developer.tistory.com/97
-  function Carousel(parentsClass = "", itemWidth = 414, moveSpeed = 500) {
-    this.list = document.querySelector(`${parentsClass}.carousel-list`);
-    this.items = document.querySelectorAll(`${parentsClass}.carousel-item`);
-    this.nextBtn = document.querySelector(`${parentsClass}.carousel-next-btn`);
-    this.prevBtn = document.querySelector(`${parentsClass}.carousel-prev-btn`);
-
-    this.len = this.items.length;
-    this.moveSpeed = moveSpeed;
-    this.itemWidth = itemWidth;
-    this.curIndex = 0;
-
-    //carousel list width 설정
-    this.list.style.width = `${itemWidth * (this.len + 2)}px`;
-
-    //first and last 엘리먼트 복사
-    this.clonedFirst = this.list.firstElementChild.cloneNode(true);
-    this.clonedLast = this.list.lastElementChild.cloneNode(true);
-
-    // carouse list 앞뒤에 복사한 엘리먼트 추가(무한 슬라이딩 기능)
-    this.list.insertBefore(this.clonedLast, this.list.firstElementChild);
-    this.list.appendChild(this.clonedFirst);
-    this.list.style.transform = `translate3d(-${itemWidth *
-      (this.curIndex + 1)}px, 0px, 0px)`;
-
-    this.curItem = this.items[this.curIndex];
-    this.curItem.classList.add("carousel-show");
-
-    /** Next Button Event */
-    if (this.nextBtn) {
-      this.nextBtn.addEventListener("click", function() {
-        this.next();
-      });
-    }
-
-    /** Prev Button Event */
-    if (this.prevBtn) {
-      this.prevBtn.addEventListener("click", function() {
-        this.prev();
-      });
-    }
-  }
-
-  Carousel.prototype.next = function() {
-    // carousel-list를 왼쪽으로 이동시킴
-    if (this.curIndex <= this.len - 1) {
-      this.list.style.transitionDuration = this.moveSpeed + "ms";
-      this.list.style.transform = `translate3d(-${this.itemWidth *
-        (this.curIndex + 2)}px, 0px, 0px)`;
-    }
-    //마지막 아이템에 도달한 경우
-    if (this.curIndex === this.len - 1) {
-      //애니메이션 없이 처음 아이템 자리로 이동시킴
-      setTimeout(() => {
-        this.list.style.transitionDuration = "0ms";
-        this.list.style.transform = `translate3d(-${this.itemWidth}px, 0px, 0px)`;
-      }, this.moveSpeed);
-      //아래서 Index가 증가하기 때문에 선처리
-      this.curIndex = -1;
-    }
-
-    //방금전 item에서 carousel-show 클래스 삭제
-    //새롭게 현재 item 설정
-    //새로운 item에 carousel-show 클래스 부여
-    this.curItem.classList.remove("carousel-show");
-    this.curItem = this.items[++this.curIndex];
-    this.curItem.classList.add("carousel-show");
-  };
-
-  Carousel.prototype.prev = function() {
-    // carousel-list를 오른쪽으로 이동시킴
-    if (this.curIndex >= 0) {
-      this.list.style.transitionDuration = this.moveSpeed + "ms";
-      this.list.style.transform = `translate3d(-${this.itemWidth *
-        this.curIndex}px, 0px, 0px)`;
-    }
-    //마지막 아이템에 도달한 경우
-    if (this.curIndex === 0) {
-      //애니메이션 없이 마지막 아이템 자리로 이동시킴
-      setTimeout(() => {
-        this.list.style.transitionDuration = "0ms";
-        this.list.style.transform = `translate3d(-${this.itemWidth *
-          this.len}px, 0px, 0px)`;
-      }, this.moveSpeed);
-      //아래서 Index가 증가하기 때문에 선처리
-      this.curIndex = this.len;
-    }
-
-    //방금전 item에서 carousel-show 클래스 삭제
-    //새롭게 현재 item 설정
-    //새로운 item에 carousel-show 클래스 부여
-    this.curItem.classList.remove("carousel-show");
-    this.curItem = this.items[--this.curIndex];
-    this.curItem.classList.add("carousel-show");
-  };
-
-  Carousel.prototype.auto = function(slideTime = 1000) {
-    if (slideTime < 1000) {
-      slideTime = 1000;
-    }
-    setInterval(() => {
-      this.next();
-    }, slideTime);
-  };
-
   //1. 네이버 로고, 예약 버튼 클릭시 반응 없애기
   $(".lnk_logo").click(function(e) {
     e.preventDefault();
@@ -129,7 +24,7 @@ $(document).ready(function() {
   //3-1. Promotion API로 데이터 가져오기
   $.ajax({
     type: "GET",
-    url: "http://localhost:8080/api/promotions",
+    url: `${URL}${PROMOTIONS_API_PATH}`,
     dataType: "JSON",
     success: function(data) {
       //3-2. Promotion Template 세팅
@@ -157,7 +52,7 @@ $(document).ready(function() {
   //4-1. Products api 가져오기
   $.ajax({
     type: "GET",
-    url: "http://localhost:8080/api/products",
+    url: `${URL}${PRODUCTS_API_PATH}`,
     dataType: "JSON",
     success: function(data) {
       //4-1. 제품 갯수 text update
@@ -230,7 +125,7 @@ $(document).ready(function() {
     const categoryId = Number(clickedEl.attr("data-category"));
 
     //5-2. categoryId별 요청 URL 세팅
-    let url = `http://localhost:8080/api/products?`;
+    let url = `${URL}${PRODUCTS_API_PATH}?`;
     if (categoryId > 0) {
       url += `categoryId=${categoryId}`;
     }
@@ -326,7 +221,7 @@ $(document).ready(function() {
       document.querySelector(".wrap_event_box").getAttribute("products-count")
     );
 
-    let url = `http://localhost:8080/api/products?start=${productsCount}`;
+    let url = `${URL}${PRODUCTS_API_PATH}?start=${productsCount}`;
     if (categoryId > 0) {
       url += `&categoryId=${categoryId}`;
     }
