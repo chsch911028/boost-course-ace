70c2ebfb11897e388657b55da02ede9ecbb1cc84
seongchanchoi <chsch911028@gmail.com>
2019-09-03 01:04:32 +0900

    Create Constants.js for constant variables

---
+++ src/main/webapp/WEB-INF/js/Constants.js
@@ -0,0 +1,11 @@
+const PROTOCOL = "http://";
+const HOST = "localhost";
+const PORT = ":8080";
+const URL = PROTOCOL + HOST + PORT;
+
+const PROMOTIONS_API_PATH = `/api/promotions`;
+const PRODUCTS_API_PATH = `/api/products`;
+
+/* Carousel */
+const CAROUSEL_DEFAULT_ITEM_WITH = 414;
+const CAROUSEL_DEFAULT_MOVE_SPEED = 500;
