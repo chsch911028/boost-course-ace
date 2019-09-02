cfde5e23b964337643130a1f9ef6488a8e02229b
seongchanchoi <chsch911028@gmail.com>
2019-09-02 18:15:38 +0900

    Add getProducts(), getProductsCount() in ProductService interface

--- src/main/java/kr/or/connect/pj3be/service/ProductService.java
+++ src/main/java/kr/or/connect/pj3be/service/ProductService.java
@@ -1,11 +1,12 @@
 package kr.or.connect.pj3be.service;
 
 import kr.or.connect.pj3be.dto.product.Product;
+import kr.or.connect.pj3be.dto.product.ProductResponse;
 
 import java.util.List;
 
 public interface ProductService {
     public static final Integer LIMIT = 4;
-    public List<Product> getList(Integer categoryId, Integer start);
-    public Integer getCount(Integer categoryId);
+    public List<Product> getProducts(Integer categoryId, Integer start);
+    public Integer getProductsCount(Integer categoryId);
 }
