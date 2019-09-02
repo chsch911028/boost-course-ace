c4f99bc2be349257bab1d64b52b019e773f5ea37
seongchanchoi <chsch911028@gmail.com>
2019-09-02 18:13:05 +0900

    Implement getAllPromotions() in PromotionServiceImpl

--- src/main/java/kr/or/connect/pj3be/service/PromotionServiceImpl.java
+++ src/main/java/kr/or/connect/pj3be/service/PromotionServiceImpl.java
@@ -1,22 +1,22 @@
 package kr.or.connect.pj3be.service;
 
 import kr.or.connect.pj3be.dao.PromotionDao;
-import kr.or.connect.pj3be.dto.promotion.Promotion;
+import kr.or.connect.pj3be.dto.promotion.PromotionResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class PromotionServiceImpl implements PromotionService {
 
     @Autowired
-    PromotionDao repository;
+    PromotionDao promotionDao;
 
-    @Transactional
+    @Transactional(readOnly = true)
     @Override
-    public List<Promotion> getList() {
-        return repository.selectAll();
+    public PromotionResponse getAllPromotions() {
+        PromotionResponse promotionResponse = new PromotionResponse();
+        promotionResponse.setItems(promotionDao.getAllPromotions());
+        return promotionResponse;
     }
 }
