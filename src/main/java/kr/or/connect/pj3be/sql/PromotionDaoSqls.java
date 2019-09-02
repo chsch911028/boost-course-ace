86ab09b39e9fce96f9c07b75741ff650146bd9da
seongchanchoi <chsch911028@gmail.com>
2019-09-02 18:10:48 +0900

    Cleanup query in PromotionDaoSqls

--- src/main/java/kr/or/connect/pj3be/sql/PromotionDaoSqls.java
+++ src/main/java/kr/or/connect/pj3be/sql/PromotionDaoSqls.java
@@ -1,5 +1,14 @@
 package kr.or.connect.pj3be.sql;
 
 public class PromotionDaoSqls {
-    public static final String SELECT_ALL="select pr.id, p.id as product_id, f.file_name as product_image_url FROM promotion pr INNER JOIN product p on pr.product_id = p.id INNER JOIN product_image img on p.id = img.product_id and img.type = \"th\" INNER JOIN file_info f on img.file_id = f.id";
+    public static final String SELECT_ALL_PROMOTIONS="SELECT pr.id, " +
+            "p.id as product_id, " +
+            "f.file_name as product_image_url " +
+            "FROM promotion pr " +
+            "INNER JOIN product p " +
+            "ON pr.product_id = p.id " +
+            "INNER JOIN product_image img " +
+            "ON p.id = img.product_id AND img.type = \"th\" " +
+            "INNER JOIN file_info f " +
+            "ON img.file_id = f.id";
 }
