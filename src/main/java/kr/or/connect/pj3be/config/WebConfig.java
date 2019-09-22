ca561fa30b531dc0f412986ca6616ee7e88a243b
seongchanchoi <chsch911028@gmail.com>
2019-09-23 02:07:41 +0900

    Update WebCofig.java for responding static file.
    [WHAT]
     - static 파일 응답 관련, 디렉토리 위치 변경
    [WHY]
     - static 파일 관련 디렉토리 위치를  WEB-INF 하위로 이동시켰기 때문에

--- src/main/java/kr/or/connect/pj3be/config/WebConfig.java
+++ src/main/java/kr/or/connect/pj3be/config/WebConfig.java
@@ -2,7 +2,6 @@ package kr.or.connect.pj3be.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.*;
 
@@ -11,28 +10,28 @@ import org.springframework.web.servlet.config.annotation.*;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final Integer ONE_HOUR_CACHE_PERIOD = 3600;
+
     @Override
     public void configureViewResolvers(ViewResolverRegistry viewResolverRegistry) {
-        viewResolverRegistry.jsp("/WEB-INF/",".jsp");
+        viewResolverRegistry.jsp("/WEB-INF/", ".jsp");
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("/WEB-INF/htmls/").setCachePeriod(31556926);
-        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/").setCachePeriod(31556926);
-        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/js/").setCachePeriod(31556926);
-        registry.addResourceHandler("/font/**").addResourceLocations("/WEB-INF/font/").setCachePeriod(31556926);
+        registry.addResourceHandler("/**").addResourceLocations("/WEB-INF/htmls/").setCachePeriod(ONE_HOUR_CACHE_PERIOD);
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/").setCachePeriod(ONE_HOUR_CACHE_PERIOD);
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/js/").setCachePeriod(ONE_HOUR_CACHE_PERIOD);
+        registry.addResourceHandler("/template/**").addResourceLocations("/WEB-INF/template/").setCachePeriod(ONE_HOUR_CACHE_PERIOD);
+        registry.addResourceHandler("/lib/**").addResourceLocations("/WEB-INF/lib/").setCachePeriod(ONE_HOUR_CACHE_PERIOD);
+        registry.addResourceHandler("/font/**").addResourceLocations("/WEB-INF/font/").setCachePeriod(ONE_HOUR_CACHE_PERIOD);
 
-        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
-        registry.addResourceHandler("/img_map/**").addResourceLocations("/img_map/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/WEB-INF/img/").setCachePeriod(ONE_HOUR_CACHE_PERIOD);
+        registry.addResourceHandler("/img_map/**").addResourceLocations("/WEB-INF/img_map/").setCachePeriod(ONE_HOUR_CACHE_PERIOD);
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping(("/**"))
-                .allowedOrigins("*")
-                .allowCredentials(false)
-                .maxAge(3600);
+        registry.addMapping(("/**")).allowedOrigins("*").allowCredentials(false).maxAge(ONE_HOUR_CACHE_PERIOD);
     }
 }
-
